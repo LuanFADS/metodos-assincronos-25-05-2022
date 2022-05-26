57c5de2434c1d39f74a2735abe38c615044349a0
Luan <luancianorte@gmail.com>
2022-05-25 22:20:59 -0300

    Second Commit

---
+++ Exemplo.Tasks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Exemplo.Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Task<string>(() => 
+            { 
+                try
+                {
+                    return "Luan";
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            });
+            
+            var result = task.GetAwaiter().GetResult();
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
